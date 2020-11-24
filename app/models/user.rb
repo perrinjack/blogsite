dd1fd01a46e5c5c5b6b61febcdc2c71a69cbd910
J Perrin <perrinjack96@gmail.com>
2020-11-24 11:29:19 +0000

    Added debise security exteansion gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Unused modules at the moment :confirmable,:omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :lockable, :timeoutable, :trackable
 end
