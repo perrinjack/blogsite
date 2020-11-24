00aa6d877bb739625c5e8ffb669a5f06bd2b032e
J Perrin <perrinjack96@gmail.com>
2020-11-24 11:17:26 +0000

    Ran migration on and added trackable module

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2020_11_23_210635) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
